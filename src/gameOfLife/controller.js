8b6392daac218663e8108893d9d3efd6c9238f96
TristanDamian <45990110+TristanDamian@users.noreply.github.com>
2020-05-30 21:49:00 +0200

    résolution bug grille

--- src/gameOfLife/controller.js
+++ src/gameOfLife/controller.js
@@ -1,3 +1,14 @@
+import { drawGame } from "./view";
+
 export const controller = model => {
-  model.run();
+  //model.run( new Date().getTime());
+  document.getElementById("start").addEventListener("click", () => {
+    model.run();
+  });
+  document.getElementById("stop").addEventListener("click", () => {
+    model.stop();
+  });
+  document.getElementById("reset").addEventListener("click", () => {
+    model.reset();
+  });
 };
--- src/gameOfLife/model.js
+++ src/gameOfLife/model.js
@@ -4,12 +4,14 @@ import {
   DEFAULT_ALIVE_PAIRS,
   RENDER_INTERVAL
 } from "./constants";
+import { drawGame } from "./view";
 
 export class Model {
-  constructor() {
+  constructor(callback) {
     this.width = GAME_SIZE;
     this.height = GAME_SIZE;
     this.raf = null;
+    this.callback = callback;
   }
 
   init() {
@@ -26,13 +28,28 @@ export class Model {
     this.raf = requestAnimationFrame(() => {
       const currentTime = new Date().getTime();
       if (currentTime - date > RENDER_INTERVAL) {
-
+        const modification = [];
         for (let i = 0; i < this.width; i++) {
           for (let j = 0; j < this.width; j++) {
             const nbAlive = this.aliveNeighbours(i, j);
+            //console.log(nbAlive);
+            if (
+              this.state[j][i] === CELL_STATES.ALIVE &&
+              (nbAlive < 2 || nbAlive > 3)
+            ) {
+              modification.push([i, j, CELL_STATES.DEAD]);
+            } else {
+              if (nbAlive == 3) {
+                modification.push([i, j, CELL_STATES.ALIVE]);
+              }
+            }
             // TODO implement Game of life logic
           }
         }
+
+        modification.forEach(([x, y, value]) => {
+          this.state[y][x] = value;
+        });
 
         this.updated();
         this.run(currentTime);
@@ -49,6 +66,10 @@ export class Model {
 
   reset() {
     // TODO
+
+    this.stop();
+    this.init();
+    this.updated();
   }
 
   isCellAlive(x, y) {
@@ -60,13 +81,51 @@ export class Model {
       ? 1
       : 0;
   }
+
   aliveNeighbours(x, y) {
     let number = 0;
     // TODO
+    /*for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        if (j !== 0 || i !== 0) {
+          //on ne test pas la cellule de départ
+          if (this.isCellAlive(x + i, y + j)) {
+            number++;
+          }
+        }
+      }
+    }*/
+
+    if (this.isCellAlive(x - 1, y - 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x, y - 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x + 1, y - 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x - 1, y)) {
+      number++;
+    }
+    if (this.isCellAlive(x + 1, y)) {
+      number++;
+    }
+    if (this.isCellAlive(x - 1, y + 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x, y + 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x + 1, y + 1)) {
+      number++;
+    }
+
     return number;
   }
 
   updated() {
     // TODO update the view
+    this.callback(this);
   }
 }
